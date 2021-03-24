b03a74fd1709bd51fef63dd6713bafb1c77b9bc8
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-03-24 11:50:39 -0400

    finish challenge 04

---
+++ node/challenges/challenge04-introducao-ao-solid/src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,28 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+class CreateUserUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ email, name }: IRequest): User {
+    const findUser = this.usersRepository.findByEmail(email);
+
+    if (findUser) {
+      throw new Error("Email already in use");
+    }
+
+    const user = this.usersRepository.create({
+      name,
+      email,
+    });
+
+    return user;
+  }
+}
+
+export { CreateUserUseCase };
