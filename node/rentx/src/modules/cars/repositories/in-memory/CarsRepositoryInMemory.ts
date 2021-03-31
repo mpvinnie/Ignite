1ad9934a29daefc9a9a1d32874c43c2b99e8ffea
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-03-31 15:24:42 -0400

    list availables cars

--- node/rentx/src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ node/rentx/src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -35,6 +35,27 @@ class CarsRepositoryInMemory implements ICarsRepository {
 
     return car
   }
+
+  public async findAvailable(
+    brand?: string,
+    category_id?: string,
+    name?: string
+  ): Promise<Car[]> {
+    const cars = this.cars.filter(car => {
+      if (
+        car.available === true ||
+        (brand && car.brand === brand) ||
+        (category_id && car.category_id === category_id) ||
+        (name && car.name === name)
+      ) {
+        return car
+      }
+
+      return null
+    })
+
+    return cars
+  }
 }
 
 export default CarsRepositoryInMemory
