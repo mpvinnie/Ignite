71bf59c9cc5a3b0064f3c41932cd9b208ac18be3
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-05-05 11:37:27 -0400

    create auth context

--- react/auth/next-jwt-auth/src/pages/index.tsx
+++ react/auth/next-jwt-auth/src/pages/index.tsx
@@ -1,9 +1,44 @@
+import { FormEvent, useState } from 'react'
+import { useAuth } from '../contexts/AuthContenxt'
+
 import styles from './home.module.scss'
 
 export default function Home() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { signIn } = useAuth()
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    const data = {
+      email,
+      password
+    }
+
+    await signIn(data)
+  }
+
   return (
     <div className={styles.container}>
-      <h1>Hello World</h1>
+      <form className={styles.formContainer} onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Entrar</button>
+      </form>
     </div>
   )
 }
