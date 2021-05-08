d4dc2b5d75812b977392b11d578e80dd8d1a2674
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-05-08 14:21:29 -0400

    create permission hook

--- react/auth/next-jwt-auth/src/pages/dashboard.tsx
+++ react/auth/next-jwt-auth/src/pages/dashboard.tsx
@@ -1,11 +1,16 @@
 import { useEffect } from "react"
 import { useAuth } from "../contexts/AuthContenxt"
+import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
 import { api } from '../services/apiClient'
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
   const { user } = useAuth()
+
+  const userCanSeeMetrics = useCan({
+    roles: ['administrator', 'editor']
+  })
 
   useEffect(() => {
     api.get('/me')
@@ -14,7 +19,11 @@ export default function Dashboard() {
   }, [])
 
   return (
-    <h1>Dashboard: {user?.email}</h1>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      { userCanSeeMetrics && <div>Métricas</div>}
+    </>
   )
 }
 
