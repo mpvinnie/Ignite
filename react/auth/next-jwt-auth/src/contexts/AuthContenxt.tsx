144f8e1ffe2256d09329af19bd593ad87a3b4ace
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-05-08 13:51:23 -0400

    server side refresh token

--- react/auth/next-jwt-auth/src/contexts/AuthContenxt.tsx
+++ react/auth/next-jwt-auth/src/contexts/AuthContenxt.tsx
@@ -2,8 +2,7 @@ import { createContext, ReactNode, useContext, useEffect, useState } from 'react
 import { toast } from 'react-toastify'
 import Router from 'next/router'
 import { setCookie, parseCookies, destroyCookie } from 'nookies'
-
-import { api } from '../services/api'
+import { api } from '../services/apiClient'
 
 type User = {
   email: string
--- react/auth/next-jwt-auth/src/pages/dashboard.tsx
+++ react/auth/next-jwt-auth/src/pages/dashboard.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react"
 import { useAuth } from "../contexts/AuthContenxt"
-import { api } from "../services/api"
+import { setupAPIClient } from "../services/api"
+import { api } from '../services/apiClient'
+import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
   const { user } = useAuth()
@@ -15,3 +17,15 @@ export default function Dashboard() {
     <h1>Dashboard: {user?.email}</h1>
   )
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx)
+
+  const response = await apiClient.get('/me')
+
+  console.log(response.data)
+
+  return {
+    props: {}
+  }
+})
