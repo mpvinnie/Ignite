392b91afa8e225dc656ab0d4087e565c3ea1f6d1
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-05-09 13:58:56 -0400

    memo; useMemo

---
+++ react/performance-react/components/SearchResults.tsx
@@ -0,0 +1,27 @@
+import { useMemo } from "react";
+import { ProductItem } from "./ProductItem";
+
+interface SearchResultsProps {
+  results: Array<{
+    id: number
+    price: number
+    title: string
+  }>
+}
+
+export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price
+    }, 0)
+  }, [results])
+
+  return (
+    <div>
+      <h2>{totalPrice}</h2>
+      {results.map(product => (
+        <ProductItem product={product} key={product.id} />
+      ))}
+    </div>
+  )
+}
