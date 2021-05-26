9051b1179ff596b41e32e78cccfd6f4493f672c4
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-05-26 11:26:58 -0400

    create header

---
+++ react-native/gofinances/src/screens/Register/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components/native'
+import { RFValue as remv } from 'react-native-responsive-fontsize'
+
+export const Container = styled.View`
+  flex: 1;
+`
+
+export const Header = styled.View`
+  background: ${({ theme }) => theme.colors.primary};
+
+  width: 100%;
+  height: ${remv(113)}px;
+
+  align-items: center;
+  justify-content: flex-end;
+  padding-bottom: 18px;
+`
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${remv(18)}px;
+`
