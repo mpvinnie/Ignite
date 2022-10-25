2b8525a37029e62ad12ab455d7f3f760d3220639
Vinicius Peres <vinniemalafaia@hotmail.com>
2022-10-25 15:41:09 -0400

    create tasks header

--- 2022/react/challenges/challenge-01-praticando-conceitos-reactjs/src/Components/Todo.tsx
+++ 2022/react/challenges/challenge-01-praticando-conceitos-reactjs/src/Components/Todo.tsx
@@ -1,8 +1,12 @@
 import { PlusCircle } from 'phosphor-react'
+import { useState } from 'react'
 
 import styles from './Todo.module.css'
 
 export function Todo() {
+  const [totalTasks, setTotalTasks] = useState(0)
+  const [completedTasks, setComplitedTasks] = useState(0)
+
   return (
     <div className={styles.container}>
       <form className={styles.searchContainer}>
@@ -13,8 +17,20 @@ export function Todo() {
         </button>
       </form>
 
-      <div>
-        TESTE
+      <div className={styles.content}>
+        <header>
+          <div className={styles.createdTasks}>
+            <strong>Tarefas criadas</strong>
+            <span>{totalTasks}</span>
+          </div>
+          <div className={styles.completedTasks}>
+            <strong>Concluídas</strong>
+            {totalTasks > 0
+              ? <span>{`${completedTasks} de ${totalTasks}`}</span>
+              : <span>0</span>
+            }
+          </div>
+        </header>
       </div>
     </div>
   )
