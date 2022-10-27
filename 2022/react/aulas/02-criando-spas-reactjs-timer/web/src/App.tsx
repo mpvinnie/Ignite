cff2a86f838933bed8d12f1731b099efd1ac5506
Vinicius Peres <vinniemalafaia@hotmail.com>
2022-10-27 10:19:54 -0400

    create global styles

--- 2022/react/aulas/02-criando-spas-reactjs-timer/web/src/App.tsx
+++ 2022/react/aulas/02-criando-spas-reactjs-timer/web/src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+
 export function App() {
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  )
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<h1>App</h1>
+			<GlobalStyle />
+		</ThemeProvider>
+	);
 }
