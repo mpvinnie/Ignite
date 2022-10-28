496ec46990d3ab7becf00dc9a65e9ef0d1e6357d
Vinicius Peres <vinniemalafaia@hotmail.com>
2022-10-28 13:18:06 -0400

    use form

--- 2022/react/aulas/02-criando-spas-reactjs-timer/web/src/pages/Home/index.tsx
+++ 2022/react/aulas/02-criando-spas-reactjs-timer/web/src/pages/Home/index.tsx
@@ -1,4 +1,8 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { NewCycleFormData, newCycleFormValidationSchema } from './home.schemas'
+
 import {
   CountdownContainer,
   FormContainer,
@@ -10,15 +14,33 @@ import {
 } from './styles'
 
 export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0
+    }
+  })
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(data)
+
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form action=''>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor='task'>Vou trabalhar em</label>
           <TaskInput
             id='task'
             list='task-suggestions'
             placeholder='Dê um nome para o seu projeto'
+            {...register('task')}
           />
 
           <datalist id='task-suggestions'>
@@ -36,6 +58,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -49,7 +72,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton type='submit'>
+        <StartCountdownButton disabled={isSubmitDisabled} type='submit'>
           <Play size={24} />
           Começar
         </StartCountdownButton>
