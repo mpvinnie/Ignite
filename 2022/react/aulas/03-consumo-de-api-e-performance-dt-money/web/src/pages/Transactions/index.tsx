1df46c23e6d71b9c7576fa5fab1b24f5bc8a54dd
Vinicius Peres <vinniemalafaia@hotmail.com>
2022-11-06 22:01:56 -0400

    create summary and transactions list

--- 2022/react/aulas/03-consumo-de-api-e-performance-dt-money/web/src/pages/Transactions/index.tsx
+++ 2022/react/aulas/03-consumo-de-api-e-performance-dt-money/web/src/pages/Transactions/index.tsx
@@ -1,11 +1,50 @@
 import { Header } from "../../components/Header";
-import { TransactionsContainer } from "./styles";
+import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsListContainer,
+  TransactionsTable
+} from "./styles";
 
 export function Transactions() {
   return (
     <TransactionsContainer>
       <Header />
-      Transactions
+      <Summary />
+
+      <TransactionsListContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>06/11/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 50,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>06/11/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Alimentação</td>
+              <td>
+                <PriceHighlight variant="outcome">-R$ 500,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>06/11/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsListContainer>
     </TransactionsContainer>
   )
 }
--- 2022/react/aulas/03-consumo-de-api-e-performance-dt-money/web/src/styles/global.ts
+++ 2022/react/aulas/03-consumo-de-api-e-performance-dt-money/web/src/styles/global.ts
@@ -12,6 +12,16 @@ export const GlobalStyle = createGlobalStyle`
     box-shadow: 0 0 0 2px ${props => props.theme["green-500"]};
   }
 
+  html {
+    @media (max-width: 960px) {
+      font-size: 87.5%;
+    }
+
+    @media (max-width: 640px) {
+      font-size: 75%;
+    }
+  }
+
   body {
     background: ${props => props.theme["gray-800"]};
     color: ${props => props.theme["gray-100"]};
@@ -22,8 +32,11 @@ export const GlobalStyle = createGlobalStyle`
     font: 400 1rem Roboto, sans-serif;
   }
 
+  button, input {
+    border: 0;
+  }
+
   button {
     cursor: pointer;
-    border: 0;
   }
 `
