793bf97011487b404ea79250225455a42873677d
Vinicius Peres <vinniemalafaia@hotmail.com>
2022-11-08 17:51:58 -0400

    performance

--- 2022/react/aulas/03-consumo-de-api-e-performance-dt-money/web/src/hooks/useSummary.ts
+++ 2022/react/aulas/03-consumo-de-api-e-performance-dt-money/web/src/hooks/useSummary.ts
@@ -1,27 +1,32 @@
-import { useContext } from 'react'
+import { useMemo } from 'react'
+import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../contexts/TransactionsContext'
 
 export function useSummary() {
-  const { transactions } = useContext(TransactionsContext)
+  const transactions = useContextSelector(TransactionsContext, context => {
+    return context.transactions
+  })
 
-  const summary = transactions.reduce(
-    (acc, transactions) => {
-      if (transactions.type === 'income') {
-        acc.income += transactions.price
-        acc.total += transactions.price
-      } else {
-        acc.outcome += transactions.price
-        acc.total -= transactions.price
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transactions) => {
+        if (transactions.type === 'income') {
+          acc.income += transactions.price
+          acc.total += transactions.price
+        } else {
+          acc.outcome += transactions.price
+          acc.total -= transactions.price
+        }
+
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0
       }
-
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0
-    }
-  )
+    )
+  }, [transactions])
 
   return summary
 }
