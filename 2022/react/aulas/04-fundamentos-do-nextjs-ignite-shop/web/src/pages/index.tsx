5813ed538586cc7487d14ba6f789b73344fb9a44
Vinicius Peres <vinniemalafaia@hotmail.com>
2022-11-13 00:31:32 -0400

    config stitches

--- 2022/react/aulas/04-fundamentos-do-nextjs-ignite-shop/web/src/pages/index.tsx
+++ 2022/react/aulas/04-fundamentos-do-nextjs-ignite-shop/web/src/pages/index.tsx
@@ -1,5 +1,24 @@
+import { styled } from '../styles'
+
+const Button = styled('button', {
+  background: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold'
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  }
+})
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Button>
+      <span>Teste</span>
+    </Button>
   )
 }
