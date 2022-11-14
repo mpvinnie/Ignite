68ade3b435a5d236ad5a7095871d9cbcc76765ca
Vinicius Peres <vinniemalafaia@hotmail.com>
2022-11-14 04:19:16 -0400

    create header component

--- 2022/react/aulas/04-fundamentos-do-nextjs-ignite-shop/web/src/pages/_app.tsx
+++ 2022/react/aulas/04-fundamentos-do-nextjs-ignite-shop/web/src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 
+import { AppContainer, Header } from '../styles/pages/app'
+
+import logoImg from '../assets/logo.svg'
+
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppContainer>
+      <Header>  
+        <img src={logoImg.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </AppContainer>
+  )
 }
