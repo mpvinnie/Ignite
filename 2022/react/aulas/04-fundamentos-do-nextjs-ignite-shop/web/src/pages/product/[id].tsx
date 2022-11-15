f8bd10f97255bac1ff311886f3d7fc8381137700
Vinicius Peres <vinniemalafaia@hotmail.com>
2022-11-15 08:28:06 -0400

    create stripe checkout api

--- 2022/react/aulas/04-fundamentos-do-nextjs-ignite-shop/web/src/pages/product/[id].tsx
+++ 2022/react/aulas/04-fundamentos-do-nextjs-ignite-shop/web/src/pages/product/[id].tsx
@@ -1,6 +1,8 @@
+import axios from 'axios'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import Image from 'next/legacy/image'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import Stripe from 'stripe'
 import { stripe } from '../../lib/stripe'
 import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
@@ -11,11 +13,31 @@ interface ProductProps {
     name: string
     imageUrl: string
     price: string
-    description: string
+    description: string,
+    defaultPriceId: string
   }
 }
 
 export default function Product({ product }: ProductProps) {
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+  
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true)
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId
+      })
+
+      const { checkoutUrl } = response.data
+
+      window.location.href = checkoutUrl
+    } catch (err) {
+      // Conectar com uma ferramenta de observabilidade (Datadog / Sentry)
+      setIsCreatingCheckoutSession(false)
+      alert('Falha ao redirecionar ao checkout!')
+    }
+  }
+  
   const { isFallback } = useRouter()
 
   if (isFallback) {
@@ -34,7 +56,10 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
       
-        <button>
+        <button
+          disabled={isCreatingCheckoutSession}
+          onClick={handleBuyProduct}
+        >
           Comprar agora
         </button>
       </ProductDetails>
@@ -73,7 +98,8 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           style: 'currency',
           currency: 'BRL'
         }).format(price.unit_amount / 100),
-        description: product.description
+        description: product.description,
+        defaultPriceId: price.id
       }
     },
     revalidate: 60 * 60 * 1 // 1 hour
