3479492d7e3ba5d861c538eb7ca3fc2fc1e41e0a
Vinicius Peres <vinniemalafaia@hotmail.com>
2022-11-16 01:11:14 -0400

    add seo to pages

--- 2022/react/aulas/04-fundamentos-do-nextjs-ignite-shop/web/src/pages/product/[id].tsx
+++ 2022/react/aulas/04-fundamentos-do-nextjs-ignite-shop/web/src/pages/product/[id].tsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { GetStaticPaths, GetStaticProps } from 'next'
+import Head from 'next/head'
 import Image from 'next/legacy/image'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
@@ -45,25 +46,31 @@ export default function Product({ product }: ProductProps) {
   }
 
   return (
-    <ProductContainer>
-      <ImageContainer>
-        <Image src={product.imageUrl} alt='' width={520} height={480} />
-      </ImageContainer>
+    <>
+      <Head>
+        <title>{product.name} | Ignite Shop</title>
+      </Head>
 
-      <ProductDetails>
-        <h1>{product.name}</h1>
-        <span>{product.price}</span>
+      <ProductContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} alt='' width={520} height={480} />
+        </ImageContainer>
 
-        <p>{product.description}</p>
-      
-        <button
-          disabled={isCreatingCheckoutSession}
-          onClick={handleBuyProduct}
-        >
-          Comprar agora
-        </button>
-      </ProductDetails>
-    </ProductContainer>
+        <ProductDetails>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
+
+          <p>{product.description}</p>
+        
+          <button
+            disabled={isCreatingCheckoutSession}
+            onClick={handleBuyProduct}
+            >
+            Comprar agora
+          </button>
+        </ProductDetails>
+      </ProductContainer>
+    </>
   )
 }
 
