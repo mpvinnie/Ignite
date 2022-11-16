6386d4793cac9824a36be37e3501b63ef55965c0
Vinicius Peres <vinniemalafaia@hotmail.com>
2022-11-16 01:05:40 -0400

    showing product and customer data to success page

--- 2022/react/aulas/04-fundamentos-do-nextjs-ignite-shop/web/src/pages/api/checkout.ts
+++ 2022/react/aulas/04-fundamentos-do-nextjs-ignite-shop/web/src/pages/api/checkout.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { GetServerSideProps, NextApiRequest, NextApiResponse } from 'next'
 import { stripe } from '../../lib/stripe'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -12,7 +12,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(400).json({ error: 'Price not provided' })
   }
 
-  const successUrl = `${process.env.NEXT_URL}/success`
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
   const cancelUrl = `${process.env.NEXT_URL}/`
 
   const checkoutSession = await stripe.checkout.sessions.create({
