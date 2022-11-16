82c08279ae5a30a90b1c2c0834e8a17ded871aaa
Vinicius Peres <vinniemalafaia@hotmail.com>
2022-11-16 07:58:51 -0400

    config typescript

--- 2022/react/aulas/05-design-system-ignite-call/packages/react/dist/index.js
+++ 2022/react/aulas/05-design-system-ignite-call/packages/react/dist/index.js
@@ -1,2 +1,22 @@
+"use strict";
+
+// ../tokens/dist/index.mjs
+var colors = {
+  white: "#fff",
+  black: "#000",
+  gray100: "#e1e1e6",
+  gray200: "#a9a9b2",
+  gray400: "#7c7c8a",
+  gray500: "#505059",
+  gray600: "#323238",
+  gray700: "#29292e",
+  gray800: "#202024",
+  gray900: "#121214",
+  ignite300: "#00b37e",
+  ignite500: "#00875f",
+  ignite700: "#015f43",
+  ignite900: "#00291d"
+};
+
 // src/index.ts
-console.log("OK");
+console.log(colors);
