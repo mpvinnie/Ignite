cef99db2648301168a0db27d9d8b2efe342491bc
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-03-26 13:51:07 -0400

    class: cofig migrations

--- 2022-2023/node/aulas/02-criando-api-restful-nodejs/src/database.ts
+++ 2022-2023/node/aulas/02-criando-api-restful-nodejs/src/database.ts
@@ -1,8 +1,15 @@
-import { knex as setupNext } from 'knex'
+import { Knex, knex as setupNext } from 'knex'
 
-export const knex = setupNext({
+export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './tmp/app.db',
+    filename: './db/app.db',
   },
-})
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+
+export const knex = setupNext(config)
