3c65d44fd7813729e6c084ec273294f00b9d7085
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-03-28 14:19:04 -0400

    class: env vars

--- 2022-2023/node/aulas/02-criando-api-restful-nodejs/src/server.ts
+++ 2022-2023/node/aulas/02-criando-api-restful-nodejs/src/server.ts
@@ -1,17 +1,27 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  // const transaction = await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: 1000,
+  //   })
+  //   .returning('*')
 
-  return tables
+  const transactions = await knex('transactions').select('*')
+
+  return transactions
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('Http Server running on http://localhost:3333')
