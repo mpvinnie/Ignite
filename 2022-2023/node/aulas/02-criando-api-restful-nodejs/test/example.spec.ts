582f304a14639366c3a8d4aa2b54cc0764598eea
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-04-03 01:33:15 -0400

    class: create 1st test with vitest

---
+++ 2022-2023/node/aulas/02-criando-api-restful-nodejs/test/example.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from 'vitest'
+
+test('it should be able to create a new transaction', () => {
+  const responseStatusCode = 201
+
+  expect(responseStatusCode).toEqual(201)
+})
