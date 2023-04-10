dfdaee87667f847742f2a0ceaa178bd165822bc7
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-04-10 01:23:39 -0400

    class: caso de uso de registro

---
+++ 2022-2023/node/aulas/03-api-solid/src/use-cases/register.ts
@@ -0,0 +1,32 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
+
+interface RegisterUserCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export async function registerUseCase({
+  name,
+  email,
+  password,
+}: RegisterUserCaseRequest) {
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: { email },
+  })
+
+  if (userWithSameEmail) {
+    throw new Error('Email already exists')
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash,
+    },
+  })
+}
