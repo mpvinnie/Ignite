f144bd34dde70c19ebd082e9a718e25f137a3739
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-04-15 12:37:43 -0400

    class: caso de uso de perfil

---
+++ 2022-2023/node/aulas/03-api-solid/src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,38 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { hash } from 'bcryptjs'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { GetUserProfileUseCase } from './get-user-profile'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfileUseCase
+
+describe('Get User Profile Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(usersRepository)
+  })
+
+  it('should be able to get user profile', async () => {
+    const createdUser = await usersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    })
+
+    expect(user.id).toEqual(createdUser.id)
+    expect(user.name).toEqual(createdUser.name)
+  })
+
+  it('should not be able to get a non existent user profile', async () => {
+    await expect(() =>
+      sut.execute({
+        userId: 'non-existent-user-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
