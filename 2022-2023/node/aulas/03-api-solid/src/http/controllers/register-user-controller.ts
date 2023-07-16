a6ffa724dd911929b03627c1a2d653ea64914cc9
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-07-16 05:09:58 -0400

    create repository interface

--- 2022-2023/node/aulas/03-api-solid/src/http/controllers/register-user-controller.ts
+++ 2022-2023/node/aulas/03-api-solid/src/http/controllers/register-user-controller.ts
@@ -1,7 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { RegisterUserUseCase } from '@/use-cases/register-user-use-case'
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 
 export async function registerUserController(
   request: FastifyRequest,
