58054981d0c06ecfac2708cfe59323fbe4c7da92
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-07-18 05:56:40 -0400

    validating check-in date

--- 2022-2023/node/aulas/03-api-solid/src/use-cases/check-in-use-case.spec.ts
+++ 2022-2023/node/aulas/03-api-solid/src/use-cases/check-in-use-case.spec.ts
@@ -1,14 +1,19 @@
 import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { CheckInUseCase } from './check-in-use-case'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { Decimal } from '@prisma/client/runtime/library'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 let checkInsRepository: InMemoryCheckInsRepository
+let gymsRepository: InMemoryGymsRepository
 let sut: CheckInUseCase
 
 describe('Check In', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
-    sut = new CheckInUseCase(checkInsRepository)
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CheckInUseCase(checkInsRepository, gymsRepository)
 
     vi.useFakeTimers()
   })
@@ -18,36 +23,82 @@ describe('Check In', () => {
   })
 
   it('should be able to check in', async () => {
+    gymsRepository.items.push({
+      id: 'gym-01',
+      title: 'Javascript Gym',
+      description: '',
+      phone: '',
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+    })
+
     const { checkIn } = await sut.execute({
       gymId: 'gym-01',
       userId: 'gym-02',
+      userLatitude: 0,
+      userLongitude: 0,
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
   })
 
+  it('should not be able to check in to a non-existing gym ', async () => {
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-01',
+        userId: 'gym-02',
+        userLatitude: 0,
+        userLongitude: 0,
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
   it('should not be able to check in twice in same day', async () => {
+    gymsRepository.items.push({
+      id: 'gym-01',
+      title: 'Javascript Gym',
+      description: '',
+      phone: '',
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+    })
+
     vi.setSystemTime(new Date(2023, 6, 18, 8, 0, 0))
 
     await sut.execute({
       gymId: 'gym-01',
       userId: 'gym-02',
+      userLatitude: 0,
+      userLongitude: 0,
     })
 
     await expect(() =>
       sut.execute({
         gymId: 'gym-01',
         userId: 'gym-02',
+        userLatitude: 0,
+        userLongitude: 0,
       }),
     ).rejects.toBeInstanceOf(Error)
   })
 
   it('should be able to check in twice but in same day', async () => {
+    gymsRepository.items.push({
+      id: 'gym-01',
+      title: 'Javascript Gym',
+      description: '',
+      phone: '',
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+    })
+
     vi.setSystemTime(new Date(2023, 6, 18, 8, 0, 0))
 
     await sut.execute({
       gymId: 'gym-01',
       userId: 'gym-02',
+      userLatitude: 0,
+      userLongitude: 0,
     })
 
     vi.setSystemTime(new Date(2023, 6, 19, 8, 0, 0))
@@ -55,6 +106,8 @@ describe('Check In', () => {
     const { checkIn } = await sut.execute({
       gymId: 'gym-01',
       userId: 'gym-02',
+      userLatitude: 0,
+      userLongitude: 0,
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
