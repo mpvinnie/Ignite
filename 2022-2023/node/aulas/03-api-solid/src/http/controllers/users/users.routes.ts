6f3cb20b32d581bd3f2c1ae16422edc9a9d03836
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-08-04 21:54:18 -0400

    implementing refresh token

--- 2022-2023/node/aulas/03-api-solid/src/http/controllers/users/users.routes.ts
+++ 2022-2023/node/aulas/03-api-solid/src/http/controllers/users/users.routes.ts
@@ -5,11 +5,13 @@ import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { authenticateUserController } from './authenticate-user-controller'
 import { registerUserController } from './register-user-controller'
 import { userProfileController } from './user-profile-controller'
+import { refreshController } from './refresh-controller'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', registerUserController)
-
   app.post('/sessions', authenticateUserController)
+
+  app.patch('/token/refresh', refreshController)
 
   // Authenticated
   app.get('/me', { onRequest: [verifyJwt] }, userProfileController)
