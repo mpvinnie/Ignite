183bec17a60717a61bdf78447b484de62d3c783c
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-08-04 21:41:36 -0400

    refresh token strategy

--- 2022-2023/node/aulas/03-api-solid/src/app.ts
+++ 2022-2023/node/aulas/03-api-solid/src/app.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import { ZodError } from 'zod'
+import fastifyCookie from '@fastify/cookie'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
 import { usersRoutes } from './http/controllers/users/users.routes'
@@ -10,7 +11,16 @@ export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
