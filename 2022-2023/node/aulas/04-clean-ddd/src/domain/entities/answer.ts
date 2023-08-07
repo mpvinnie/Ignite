6c532cafd372f86721745716a5b6b013da6d0043
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-08-07 05:07:55 -0400

    create all getters and setters

--- 2022-2023/node/aulas/04-clean-ddd/src/domain/entities/answer.ts
+++ 2022-2023/node/aulas/04-clean-ddd/src/domain/entities/answer.ts
@@ -11,10 +11,6 @@ interface AnswerProps {
 }
 
 export class Answer extends Entity<AnswerProps> {
-  get content() {
-    return this.props.content
-  }
-
   static create(
     props: Optional<AnswerProps, 'createdAt'>,
     id?: UniqueEntityID
@@ -28,5 +24,38 @@ export class Answer extends Entity<AnswerProps> {
     )
 
     return answer
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get questionId() {
+    return this.props.questionId
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...')
   }
 }
