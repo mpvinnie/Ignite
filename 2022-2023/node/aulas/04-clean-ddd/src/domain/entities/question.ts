1d7252b0248286587e63a7d8a0ea58e87c43182c
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-08-07 03:57:01 -0400

    create slug value object

--- 2022-2023/node/aulas/04-clean-ddd/src/domain/entities/question.ts
+++ 2022-2023/node/aulas/04-clean-ddd/src/domain/entities/question.ts
@@ -1,8 +1,10 @@
 import { randomUUID } from "node:crypto"
+import { Slug } from "./value-objects/slug"
 
 interface QuestionProps {
   title: string
   content: string
+  slug: Slug
   authorId: string
 }
 
@@ -10,14 +12,15 @@ export class Question {
   public id: string
   public title: string
   public content: string
+  public slug: Slug
 
   public authorId: string
 
   constructor(props: QuestionProps, id?: string) {
     this.id = id ?? randomUUID()
-    
     this.title = props.title
     this.content = props.content
+    this.slug = props.slug
     this.authorId = props.authorId
   }
 }
