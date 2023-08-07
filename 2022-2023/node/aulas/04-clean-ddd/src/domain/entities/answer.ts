b826994c8e02d5a22ebbedf47c4b6836f7ab88f2
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-08-07 00:16:49 -0400

    mapping relashionships

--- 2022-2023/node/aulas/04-clean-ddd/src/domain/entities/answer.ts
+++ 2022-2023/node/aulas/04-clean-ddd/src/domain/entities/answer.ts
@@ -1,11 +1,22 @@
 import { randomUUID } from "node:crypto"
 
+interface AnswerProps {
+  content: string
+  authorId: string
+  questionId: string
+}
+
 export class Answer {
   public id: string
   public content: string
 
-  constructor(content: string, id?: string) {
+  public authorId: string
+  public questionId: string
+
+  constructor(props: AnswerProps, id?: string) {
     this.id = id ?? randomUUID()
-    this.content = content
+    this.content = props.content
+    this.authorId = props.authorId
+    this.questionId = props.questionId
   }
 }
