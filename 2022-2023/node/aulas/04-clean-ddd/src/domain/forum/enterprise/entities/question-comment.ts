4b3af42164f6d38708a9f33f2e3edfea321b30b9
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-08-11 05:10:29 -0400

    create comment class

--- 2022-2023/node/aulas/04-clean-ddd/src/domain/forum/enterprise/entities/question-comment.ts
+++ 2022-2023/node/aulas/04-clean-ddd/src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,16 +1,12 @@
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
+import { Comment, CommentProps } from './comment'
 
-export interface QuestionCommentProps {
-  authorId: UniqueEntityID
+export interface QuestionCommentProps extends CommentProps {
   questionId: UniqueEntityID
-  content: string
-  createdAt: Date
-  updatedAt?: Date
 }
 
-export class QuestionComment extends Entity<QuestionCommentProps> {
+export class QuestionComment extends Comment<QuestionCommentProps> {
   static create(
     props: Optional<QuestionCommentProps, 'createdAt'>,
     id?: UniqueEntityID
@@ -26,28 +22,7 @@ export class QuestionComment extends Entity<QuestionCommentProps> {
     return questionComment
   }
 
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
-
-  get authorId() {
-    return this.props.authorId
-  }
-
-  get content() {
-    return this.props.content
-  }
-
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
+  get questionId() {
+    return this.props.questionId
   }
 }
