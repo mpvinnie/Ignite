c67b9d5a9875b14157668fdf7944f4b095564052
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-08-11 04:47:51 -0400

    feat: fetch question answers

---
+++ 2022-2023/node/aulas/04-clean-ddd/src/domain/forum/application/use-cases/fetch-question-answers.spec.ts
@@ -0,0 +1,57 @@
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { makeAnswer } from 'test/factories/make-answer'
+import { FetchQuestionAnsersUseCase } from './fetch-question-answers'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+let answersRepository: InMemoryAnswersRepository
+let sut: FetchQuestionAnsersUseCase
+
+describe('Fetch question answers', () => {
+  beforeEach(() => {
+    answersRepository = new InMemoryAnswersRepository()
+
+    sut = new FetchQuestionAnsersUseCase(answersRepository)
+  })
+
+  it('should be able to fetch question answers', async () => {
+    await answersRepository.create(
+      makeAnswer({
+        questionId: new UniqueEntityID('question-1')
+      })
+    )
+    await answersRepository.create(
+      makeAnswer({
+        questionId: new UniqueEntityID('question-1')
+      })
+    )
+    await answersRepository.create(
+      makeAnswer({
+        questionId: new UniqueEntityID('question-1')
+      })
+    )
+
+    const { answers } = await sut.execute({
+      questionId: 'question-1',
+      page: 1
+    })
+
+    expect(answers).toHaveLength(3)
+  })
+
+  it('should be able to fetch paginated question answers', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await answersRepository.create(
+        makeAnswer({
+          questionId: new UniqueEntityID('question-1')
+        })
+      )
+    }
+
+    const { answers } = await sut.execute({
+      questionId: 'question-1',
+      page: 2
+    })
+
+    expect(answers).toHaveLength(2)
+  })
+})
---
+++ 2022-2023/node/aulas/04-clean-ddd/src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -0,0 +1,29 @@
+import { Answer } from '../../enterprise/entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
+
+interface FetchQuestionAnsersUseCaseRequest {
+  questionId: string
+  page: number
+}
+
+interface FetchQuestionAnsersUseCaseResponse {
+  answers: Answer[]
+}
+
+export class FetchQuestionAnsersUseCase {
+  constructor(private answersRepository: AnswersRepository) {}
+
+  async execute({
+    questionId,
+    page
+  }: FetchQuestionAnsersUseCaseRequest): Promise<FetchQuestionAnsersUseCaseResponse> {
+    const answers = await this.answersRepository.findManyByQuestionId(
+      questionId,
+      { page }
+    )
+
+    return {
+      answers
+    }
+  }
+}
