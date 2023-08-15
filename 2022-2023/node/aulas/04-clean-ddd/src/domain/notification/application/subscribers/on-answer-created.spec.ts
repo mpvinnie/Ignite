1eea5d1be5b557e1a73114036a6f59388291128d
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-08-14 21:40:44 -0400

    listening a domain event

---
+++ 2022-2023/node/aulas/04-clean-ddd/src/domain/notification/application/subscribers/on-answer-created.spec.ts
@@ -0,0 +1,24 @@
+import { makeAnswer } from 'test/factories/make-answer'
+import { OnAnswerCreated } from './on-answer-created'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
+
+let answerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
+let answersRepository: InMemoryAnswersRepository
+
+describe('On answer created', () => {
+  beforeEach(() => {
+    answerAttachmentsRepository = new InMemoryAnswerAttachmentsRepository()
+    answersRepository = new InMemoryAnswersRepository(
+      answerAttachmentsRepository
+    )
+  })
+
+  it('should send a notification when an answer is created', async () => {
+    const _answerCreated = new OnAnswerCreated()
+
+    const answer = makeAnswer()
+
+    answersRepository.create(answer)
+  })
+})
--- 2022-2023/node/aulas/04-clean-ddd/test/repositories/in-memory-answers-repository.ts
+++ 2022-2023/node/aulas/04-clean-ddd/test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/paginations-params'
 import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
@@ -30,6 +31,8 @@ export class InMemoryAnswersRepository implements AnswersRepository {
 
   async create(answer: Answer): Promise<void> {
     this.items.push(answer)
+
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 
   async delete(answer: Answer) {
@@ -44,5 +47,7 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     const itemIndex = this.items.findIndex(item => item.id === answer.id)
 
     this.items[itemIndex] = answer
+
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 }
