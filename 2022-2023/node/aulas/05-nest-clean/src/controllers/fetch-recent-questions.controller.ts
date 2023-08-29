a5621da85984f49804fd129ac862943804c6c55d
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-08-28 21:29:27 -0400

    create questions tests

--- 2022-2023/node/aulas/05-nest-clean/src/controllers/fetch-recent-questions.controller.ts
+++ 2022-2023/node/aulas/05-nest-clean/src/controllers/fetch-recent-questions.controller.ts
@@ -20,11 +20,11 @@ export class FetchRecentQuestionsController {
 
   @Get()
   async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
-    const perPage = 1
+    const perPage = 20
 
     const questions = await this.prisma.question.findMany({
       take: perPage,
-      skip: (page - perPage) * perPage,
+      skip: (page - 1) * perPage,
       orderBy: {
         createdAt: 'desc'
       }
