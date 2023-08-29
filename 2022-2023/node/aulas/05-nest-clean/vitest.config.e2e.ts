95661a46119c7d6cd8d3bb66901637d2850d02e7
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-08-28 20:58:30 -0400

    config isolated test database

--- 2022-2023/node/aulas/05-nest-clean/vitest.config.e2e.ts
+++ 2022-2023/node/aulas/05-nest-clean/vitest.config.e2e.ts
@@ -6,7 +6,8 @@ export default defineConfig({
   test: {
     include: ['**/*.e2e-spec.ts'],
     globals: true,
-    root: './src'
+    root: './src',
+    setupFiles: ['../test/setup-e2e.ts']
   },
   plugins: [
     tsConfigPaths(),
