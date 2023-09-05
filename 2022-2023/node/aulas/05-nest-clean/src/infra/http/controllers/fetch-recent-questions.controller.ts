4a118972fdb4cfc97a7c1debbd7db79b9bf3188f
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-09-05 14:11:19 -0400

    list recent question with presenter

--- 2022-2023/node/aulas/05-nest-clean/src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ 2022-2023/node/aulas/05-nest-clean/src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get, Query } from '@nestjs/common'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation.pipe'
 import { z } from 'zod'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
+import { QuestionPresenter } from '../presenters/question-presenter'
 
 const pageQueryParamSchema = z
   .string()
@@ -16,20 +17,20 @@ type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
 
 @Controller('/questions')
 export class FetchRecentQuestionsController {
-  constructor(private prisma: PrismaService) {}
+  constructor(private fetchRecentQuestions: FetchRecentQuestionsUseCase) {}
 
   @Get()
   async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
-    const perPage = 20
-
-    const questions = await this.prisma.question.findMany({
-      take: perPage,
-      skip: (page - 1) * perPage,
-      orderBy: {
-        createdAt: 'desc'
-      }
+    const result = await this.fetchRecentQuestions.execute({
+      page
     })
 
-    return { questions }
+    if (result.isLeft()) {
+      throw new Error()
+    }
+
+    const { questions } = result.value
+
+    return { questions: questions.map(QuestionPresenter.toHTTP) }
   }
 }
