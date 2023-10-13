d6310c26e7a05991336a791194439fe80b227b47
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-10-13 05:32:08 -0400

    feat: comment on answer controller

--- 2022-2023/node/aulas/05-nest-clean/src/infra/http/http.module.ts
+++ 2022-2023/node/aulas/05-nest-clean/src/infra/http/http.module.ts
@@ -29,6 +29,8 @@ import { CommentOnQuestionController } from './controllers/comment-on-question.c
 import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question'
 import { DeleteQuestionCommentController } from './controllers/delete-question-comment.controller'
 import { DeleteQuestionCommentUseCase } from '@/domain/forum/application/use-cases/delete-question-comment'
+import { CommentOnAnswerController } from './controllers/comment-on-answer.controller'
+import { CommentOnAnswerUseCase } from '@/domain/forum/application/use-cases/comment-on-answer'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -46,7 +48,8 @@ import { DeleteQuestionCommentUseCase } from '@/domain/forum/application/use-cas
     FetchQuestionAnswersController,
     ChooseQuestionBestAnswerController,
     CommentOnQuestionController,
-    DeleteQuestionCommentController
+    DeleteQuestionCommentController,
+    CommentOnAnswerController
   ],
   providers: [
     CreateQuestionUseCase,
@@ -62,7 +65,8 @@ import { DeleteQuestionCommentUseCase } from '@/domain/forum/application/use-cas
     FetchQuestionAnswersUseCase,
     ChooseQuestionBestAnswerUseCase,
     CommentOnQuestionUseCase,
-    DeleteQuestionCommentUseCase
+    DeleteQuestionCommentUseCase,
+    CommentOnAnswerUseCase
   ]
 })
 export class HttpModule {}
