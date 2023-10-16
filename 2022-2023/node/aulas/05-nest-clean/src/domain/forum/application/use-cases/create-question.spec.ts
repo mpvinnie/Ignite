70cc3852d1b604afb806ade7a68dc668595ed4e5
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-10-16 01:05:23 -0400

    create and edit question attachments

--- 2022-2023/node/aulas/05-nest-clean/src/domain/forum/application/use-cases/create-question.spec.ts
+++ 2022-2023/node/aulas/05-nest-clean/src/domain/forum/application/use-cases/create-question.spec.ts
@@ -35,4 +35,26 @@ describe('Create question', () => {
       expect.objectContaining({ attachmentId: new UniqueEntityID('2') })
     ])
   })
+
+  it('should persist attachments when creating a new question', async () => {
+    const result = await sut.execute({
+      authorId: '1',
+      title: 'Question title',
+      content: 'Question content',
+      attachmentsIds: ['1', '2']
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(questionAttachmentsRepository.items).toHaveLength(2)
+    expect(questionAttachmentsRepository.items).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          attachmentId: new UniqueEntityID('1')
+        }),
+        expect.objectContaining({
+          attachmentId: new UniqueEntityID('2')
+        })
+      ])
+    )
+  })
 })
--- 2022-2023/node/aulas/05-nest-clean/src/domain/forum/application/use-cases/edit-question.spec.ts
+++ 2022-2023/node/aulas/05-nest-clean/src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -86,4 +86,47 @@ describe('Edit question', () => {
     expect(result.isLeft()).toBe(true)
     expect(result.value).toBeInstanceOf(NotAllowedError)
   })
+
+  it('should sync and removed attachments when aditing a question', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-1')
+      },
+      new UniqueEntityID('question-1')
+    )
+
+    await questionsRepository.create(newQuestion)
+
+    questionAttachmentsRepository.items.push(
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityID('1')
+      }),
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityID('2')
+      })
+    )
+
+    const result = await sut.execute({
+      authorId: 'author-1',
+      questionId: newQuestion.id.toValue(),
+      title: 'New test title',
+      content: 'New test content',
+      attachmentsIds: ['1', '3']
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(questionAttachmentsRepository.items).toHaveLength(2)
+    expect(questionAttachmentsRepository.items).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          attachmentId: new UniqueEntityID('1')
+        }),
+        expect.objectContaining({
+          attachmentId: new UniqueEntityID('3')
+        })
+      ])
+    )
+  })
 })
