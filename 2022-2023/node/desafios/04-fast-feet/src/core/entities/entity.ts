c368037a3c6700ff7faea3de21a53386a4fc8139
Vinicius Peres <vinniemalafaia@hotmail.com>
2023-12-12 11:01:30 -0400

    create domain events structure

--- 2022-2023/node/desafios/04-fast-feet/src/core/entities/entity.ts
+++ 2022-2023/node/desafios/04-fast-feet/src/core/entities/entity.ts
@@ -9,6 +9,18 @@ export abstract class Entity<Props> {
     this._id = id ?? new UniqueEntityId(id)
   }
 
+  public equals(entity: Entity<any>) {
+    if (entity === this) {
+      return true
+    }
+
+    if (entity.id === this._id) {
+      return true
+    }
+
+    return false
+  }
+
   get id() {
     return this._id
   }
